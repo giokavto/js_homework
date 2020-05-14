3c2f55ad2c1b7a212c4fa9591d934f4442ba0e53
giokavtara <giorgi.kavtaradze@geolab.edu.ge>
2020-05-14 15:34:00 +0400

    add js_homework_1

---
+++ homework_1.js
@@ -0,0 +1,48 @@
+// lesson 1
+/*
+homework_1.js
+author: giorgi kavtaradze
+*/
+
+// homework 1.1
+
+// let i = 1;
+// let sum = 0;
+// while(i < 10) {
+//    if (i % 2 == 0) {
+//       sum = sum + i;
+//    }
+//    i = i + 1;
+   
+// }
+// console.log(sum);
+
+let sum = 0;
+
+for(let i = 1; i < 10; i++){
+    if(i % 2 == 0){
+        sum = sum + i;
+    }
+}
+console.log(sum);
+
+// homework 1.2
+
+// let j = 1;
+// let sum2 = 0;
+
+// while(j < 50){
+//     if(j % 3 ==0){
+//         sum2 = sum2 + j;
+//     }
+//     j = j + 1;
+// }
+// console.log(sum2);
+
+let sum2 = 0;
+for(j = 1; j < 50; j++){
+    if(j % 3 == 0){
+        sum2 = sum2 + j;
+    }
+}
+console.log(sum2);
