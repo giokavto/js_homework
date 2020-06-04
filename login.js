379bd036713fe78c6f72865ee34440e42fb3e23f
giokavtara <giorgi.kavtaradze@geolab.edu.ge>
2020-06-04 14:10:20 +0400

    add email and password validator

--- login.js
+++ login.js
@@ -3,19 +3,20 @@
 
 let user1 = {
     username: "giorgi1994",
-    password: "1423",
     email: "giorgi1994@gmail.com",
+    password: "1423AR$",
+    
   };
   
   let user2 = {
     username: "spophia1990",
-    password: "12374",
+    password: "f12374G&",
     email: "spophia1990@gmail.com",
   };
   
   let user3 = {
     username: "joni1988",
-    password: "1334",
+    password: "e1334LJ*",
     email: "joni1988@gmail.com",
   };
   
@@ -27,7 +28,7 @@ let user1 = {
   
   let user5 = {
     username: "lazare1995",
-    password: "157467",
+    password: "1574g67L%",
     email: "lazare1995@gmail.com",
   };
   
@@ -41,7 +42,7 @@ let user1 = {
     }
   }
 
-  function getFoundEmail(email, possibleEmail) {
+  function getFoundEmail(users, possibleEmail) {
     for (i = 0; i < users.length; i++) {
       if (users[i].email === possibleEmail) {
         return users[i];
@@ -54,27 +55,23 @@ let user1 = {
     return isSame;
   }
   
-  //////////////////////
-  $(document).on('click', '.toggle-password', function() {
-
-    $(this).toggleClass("fa-eye fa-eye-slash");
-    
-    var input = $("#password");
-    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
-});
+  
 ////////////////////////////
 
-  function login(username, password) {
-    let possibleUser = getFoundUser(users, username);
-    if (!possibleUser) {
-      console.log("მომხმარებელი ამ სახელით არ მოიძებნა");
-      alert("მომხმარებელი ამ სახელით არ მოიძებნა");
+  function login(email, password) {
+    let passwordStrong = /^.*(?=.*[A-Z]{1,})(?=.*[^\w]{1,})(?=.*[0-9]{1,})(?=.*[\w]).{6,20}$/;
+    let possibleE = getFoundEmail(users, email);
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!possibleE ?? mailformat) {
+      console.log("ამ მეილით მომხმარებელი ვერ მოიძებნა ან მეილი არასწორ ფორმატში გაქვთ შეყვანილი");
+      alert("ამ მეილით მომხმარებელი ვერ მოიძებნა ან მეილი არასწორ ფორმატში გაქვთ შეყვანილი");
     } else {
-      let isPasswordValid = checkPassword(possibleUser, password);
+      let isPasswordValid = checkPassword(possibleE, password,);
       if (isPasswordValid) {
         console.log("წარმატებით შეხვედით სისტემაში .");
         alert("წარმატებით შეხვედით სისტემაში .");
-        if (password.length < 8) {
+        window.location = "index.html";
+        if (!passwordStrong) {
           console.log(
             "მაგრამ თქვენ გაქვთ მარტივი პაროლი , გთხოვთ შეცვალოთ პაროლი"
           );
@@ -86,84 +83,59 @@ let user1 = {
         alert("პაროლი არასწორია");
       }
     }
+    
   }
-  
+  //////////////////////
+  $(document).on('click', '.toggle-password', function() {
 
-function tryLogin() {
-    let username = document.getElementById("username").value;
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    
+    let input = $("#password");
+    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+});
+
+let  tryLogin = () => {
+    // let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
+    
 
-    login(username, password);
+    login(email, password,);
 }
   
 
 
-// let username = "giorgi1994";
-// let password = "1234";
 
-
-// let user1 = {
-//     username = "giorgi1994",
-//     password = "1234",
-// };
-// let user2 = {
-//     username = "spophia1990",
-//     password = "1233",
-// };
-// let user3 = {
-//     username = "katerina1989",
-//     password = "1232",
-// };
-// let user4 = {
-//     username = "aleqsandre1992",
-//     password = "1231",
-// };
-// let user5 = {
-//     username = "lazare1995",
-//     password = "1235",
-// };
-
-// let users = [user1, user2, user3, user4, user5,];
-
-
-
-// function getFoundUser(users, possibleUsername) {
-//     // let FoundUser;
-//     for(let i = 0; i < users.lenght; i++) {
-//         if(users[i].username === possibleUsername){
-//             return users[i];
-//         }
+// function login(username, password) {
+//   let possibleUser = getFoundUser(users, username);
+//   if (!possibleUser) {
+//     console.log("მომხმარებელი ამ სახელით არ მოიძებნა");
+//     alert("მომხმარებელი ამ სახელით არ მოიძებნა");
+//   } else {
+//     let possibleEmail = getFoundEmail(email, possibleEmail);
+//     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     if(!possibleEmail ?? !mailformat){
+//       console.log("ამ მეილით მომხმარებელი ვერ მოიძებნა ან მეილი არასწორ ფორმატში გაქვთ შეყვანილი");
+//       alert("ამ მეილით მომხმარებელი ვერ მოიძებნა ან მეილი არასწორ ფორმატში გაქვთ შეყვანილი");
 //     }
-// }
-
-// function checkPassword(user, possiblePassword) {
-//     let isSame = user.password === possiblePassword;
-//     return isSame;
-// }
-
-// function isPasswordStrong(password){
-//     return password.length > 8;
-// }
-
-// function login(username, password){
-//     let possibleUser = getFoundUser(users, username);
-//     if(!possibleUser){
-//         console.log("მომხმარებელი ამ სახელით არ მოიძებნა");
+//   } else {
+//     let isPasswordValid = checkPassword(possibleUser, possibleEmail, mailformat,  password,);
+//     if (isPasswordValid) {
+//       console.log("წარმატებით შეხვედით სისტემაში .");
+//       alert("წარმატებით შეხვედით სისტემაში .");
+//       if (password.length < 8) {
+//         console.log(
+//           "მაგრამ თქვენ გაქვთ მარტივი პაროლი , გთხოვთ შეცვალოთ პაროლი"
+//         );
+//         alert("მაგრამ თქვენ გაქვთ მარტივი პაროლი , გთხოვთ შეცვალოთ პაროლი");
+//         window.location = "index.html";
+//       }
 //     } else {
-//         let isPasswordValid = checkPassword(possibleUser, password);
-//         if(isPasswordValid){
-//             if(isPasswordStrong(password)) {
-//                 console.log("წარმატებით შეხვედით სისტემაში, უბრალოდ გთხოვთ შეცვალოთ პაროლი");
-//             } else {
-//                 console.log("წარმატებით შეხვედით სისტემაში");
-//             }
-//         }   else {
-//             console.log("პაროლი არასწორია");
-//         }
+//       console.log("პაროლი არასწორია");
+//       alert("პაროლი არასწორია");
 //     }
+//   }
+  
 // }
 
-// function tryLogin() {
-//     login("giorgi1994", "1234");
-// }
-// login("giorgi1994", "1234");
+
