ce025123fff55785a9b77d6384854586dc5112a6
giokavto <37060070+giokavto@users.noreply.github.com>
2020-06-21 23:19:36 +0400

    add algorithm

---
+++ algorithm/algorithms.js
@@ -0,0 +1,71 @@
+function Graph(v){
+    this.vertices = v;
+    this.edges = 0;
+    this.adj = [];
+    for(let i = 0; i < this.vertices; i++){
+        this.adj[i] = [];
+        this.adj[i].push("");
+    }
+    this.addEdge = addEdge;
+    this.showGraph = showGraph;
+    this.bfs = bfs;
+    this.marked = [];
+    for(let i = 0; i < this.vertices; i++){
+        this.marked[i] = false;
+    }
+
+}
+
+function addEdge(v,w){
+    this.adj[v].push(w);
+    this.adj[w].push(v);
+    this.edges++;
+}
+
+function showGraph(){
+    for(let i = 0; i < this.vertices; i++){
+        console.log(i + " -> ");
+        for(let j = 0; j < this.vertices; j++){
+            if(this.adj[i][j]!=undefined)
+            console.log(this.adj[i][j]+ "");
+        }
+        console.log();
+    }
+}
+
+function bfs(v){
+    this.marked[v] = true;
+    if(this.adj[v] !=undefined){
+        console.log("Visited Vertex: " +v);
+    }
+
+    let w = "";
+    for( w in this.adj[v]) {
+        if(!this.marked[w]){
+            this.bfs(w);
+        }
+    }
+}
+
+
+
+
+g= new Graph(5);
+g.addEdge(0,1);
+g.addEdge(0,2);
+g.addEdge(1,3);
+g.addEdge(2,4);
+g.showGraph();
+g.bfs(0);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ algorithms.js
@@ -0,0 +1,13 @@
+// function(a,b){
+//     let f = 1;
+//     let c = -17.22;
+
+// }
+
+function recur(x){
+    if(x===0){
+        return;
+    }
+    recur(x-1);
+}
+console.log(recurer(8));
